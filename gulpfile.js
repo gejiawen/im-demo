bb376bfd929258d3e2e4f24f6db36dd39de26274
gejiawen <806717031@qq.com>
2017-01-13 02:21:38 +0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * @file: gulpfile
+ * @author: gejiawen
+ * @date: 13/01/2017 00:14
+ * @description: gulpfile
+ */
+
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('stylus-with-sourcemaps', () => {
+    return gulp.src('./*.styl')
+        .pipe(sourcemaps.init())
+        .pipe(stylus())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./build'))
+})
+
+gulp.task('default', ['stylus-with-sourcemaps'])
+
+gulp.watch('./*.styl', ['stylus-with-sourcemaps'])
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file: index
+ * @author: gejiawen
+ * @date: 12/01/2017 23:49
+ * @description: index
+ */
+
+$(function () {
+
+})
