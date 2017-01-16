d1a9acaa55d0205f6ca040ccd571709012ad9a16
gejiawen <806717031@qq.com>
2017-01-17 02:47:54 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # IM Demo
+
+1. your need node.js envirnoment
+2. npm install
+3. gulp serve
+4. access localhost:8080
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@
 const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const sourcemaps = require('gulp-sourcemaps')
+const exec = require('child_process').exec
 
 gulp.task('stylus-with-sourcemaps', () => {
     return gulp.src('./*.styl')
@@ -18,5 +19,18 @@ gulp.task('stylus-with-sourcemaps', () => {
 })
 
 gulp.task('default', ['stylus-with-sourcemaps'])
+gulp.task('serve', ['stylus-with-sourcemaps'], () => {
+    exec('hs .', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
+
+        console.log(`${stdout}`)
+    })
+})
 
 gulp.watch('./*.styl', ['stylus-with-sourcemaps'])
